c9deb5a2d364d3100dd5418feb1f2fca58a18988
RaphGL <raphfl.dev@gmail.com>
2022-08-02 21:16:52 +0100

    added projects and fixed issue with tailwind not updating css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./layouts/**/*",
-    "./docs/**/*.html",
-    "./content/**/*.md",
-  ],
+  content: ["./content/**/*.{html,js}", "./layouts/**/*.{html,js}"],
   theme: {
-    extend: {},
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#f8f8f2',
-      'blue': '#6483e6',
-      'light-black': '#23242b',
-      'black': '#18191f',
-      'blacker': '#000000',
-      'light-gray': '#b3b3b5',
-      'gray': '#282a36',
-      'dark-gray': '#14151b',
-      'accent-color': '#383a59'
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#f8f8f2",
+      blue: "#6483e6",
+      "light-black": "#23242b",
+      black: "#18191f",
+      blacker: "#000000",
+      "light-gray": "#b3b3b5",
+      gray: "#282a36",
+      "dark-gray": "#14151b",
+      "accent-color": "#383a59",
     },
   },
   plugins: [],
-}
+};
