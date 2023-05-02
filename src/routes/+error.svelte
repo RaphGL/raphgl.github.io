6c6e35ecbcdc67bfe1a43eb7832a87637ead1e66
RaphGL <raphfl.dev@gmail.com>
2023-05-02 20:56:44 -0100

    added 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,24 @@
+<section>
+	<div>
+		<h1>404</h1>
+		<p>Page not found</p>
+	</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+		justify-content: center;
+		align-content: center;
+		padding-bottom: 4em;
+		margin: 1em;
+		height: 70vh;
+	}
+
+	h1 {
+		font-size: 100pt;
+		margin-bottom: 0;
+	}
+</style>
